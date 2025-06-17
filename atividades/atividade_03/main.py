da1fde2df4c2db6d3bd6aec761c2fbd2cfd9d4df
gabriel17868 <ead@academico.local>
2025-06-17 19:58:44 -0300

    fim atividade 03

--- atividades/atividade_03/main.py
+++ atividades/atividade_03/main.py
@@ -9,30 +9,45 @@ import math as m
 import os
 
 while True:
+    print(" ")
+    print(f"{'-'*20} MENU{'-'*20}")
+    print("1 - Calcular área de um círculo")
+    print("2 - Calcular tamanho de uma circunferência")
+    print("3 = Sair do programa")
+    opcao = input("Informe a operação desejada: ").strip()
+    os.system("cls" if os.name =="nt" else "clear")
     try:
-        print(f"{'-'*20} MENU{'-'*20}\n")
-        print(f"1 - Calcular área de um círculo")
-        print(f"2 - Calcular tamanho de uma circunferência")
-        operador = input("Informe a operação desejada: ").strip()
-        match operador:
+        if opcao == "1" or opcao =="2":
+            r = float(input("Imforme o valor do raio em cm: ").replace(",","."))
+        else:
+            pass
+        os.system("cls" if os.name =="nt" else "clear")
+        match opcao:
             case "1":
-                try:
-                    r = float(input("Imforme o valor do raio em cm: "))
-                    area = m.pi * (r**2)
-                    print(f"O valor da área do círculo com o raio informado de {r} é de {area:.1f}cm")
-                except Exception as e:
-                    print(f"Não foi possível somar. {e}.")
-                break
+                area = m.pi * (r**2)
+                print(" ")
+                print(f"O valor da área do círculo com o raio informado de {r}cm, é de {area:.1f}cm")
+                continue
             case "2":
-                try:
-                    r = float(input("Imforme o valor do raio em cm: "))
-                    cir = (2*m.pi) * r
-                    print(f"O valor da circunferência com o raio informado de {r} é de {cir:.1f}cm")
-                except Exception as e:
-                    print(f"Não foi possível somar. {e}.")
+                cir = (2*m.pi) * r
+                print(" ")
+                print(f"O valor da circunferência com o raio informado de {r}cm, é de {cir:.1f}cm")
+                continue
+            case "3":
+                print("Você saiu do programa...")
                 break
+            case _:
+                os.system("cls" if os.name =="nt" else "clear")
+                print("Operador Inválido")
+                continue
+    except Exception as e:
+            print(f"Não foi possível calcular. {e}.")
+            continue
+    
 
-        while True:
+
+
+    '''while True:
             prosseguir = input("Deseja refazer? (s/n): ").strip().lower()
             if prosseguir == "s" or prosseguir == "n":
                 break
@@ -40,7 +55,18 @@ while True:
                 print("Opção inválida, por favor digite 's' para sim ou 'n' para não.")
             continue
 
-        match prosseguir:
+            if prosseguir == "s":
+                os.system("cls")
+                continue
+            elif prosseguir== "n":
+                os.system("cls")
+                break
+            else:
+                print("Opção inválida.")
+                break
+
+
+       match prosseguir:
             case "s":
                 os.system("cls")
                 continue
@@ -49,8 +75,4 @@ while True:
                 break
             case _:
                 print("Opção inválida.")
-                break
-
-    except Exception as e:
-            print(f"Não foi possível calcular o IMC. {e}.")
-            continue
+                break '''
