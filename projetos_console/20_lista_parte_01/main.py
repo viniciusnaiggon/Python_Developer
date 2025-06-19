f93d6c5661362dfa6823141f41d728598e96a092
gabriel17868 <ead@academico.local>
2025-06-18 21:31:09 -0300

    aula do dia 18/06/2025

---
+++ projetos_console/20_lista_parte_01/main.py
@@ -0,0 +1,15 @@
+# lista
+nomes = ["Fulano", "Cicrano", "Beltrano", "João", "Maria, José"]
+
+# ordena a lista
+nomes.sort(reverse=True) # Inverte a ordem alfabetica, de Z a A.
+
+# imprime a lista na tela
+print(nomes[0])
+print(nomes[3])
+for nome in nomes:
+    print(nome)
+
+# imprime o número de itens da lista
+print(len(nomes))
+
