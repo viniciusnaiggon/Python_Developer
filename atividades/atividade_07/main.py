7f9304f7d0b7d25b7659ad0022a184d24e6da94e
gabriel17868 <ead@academico.local>
2025-06-23 21:44:56 -0300

    Desáfio da aula do dia 23/06/2025

---
+++ atividades/atividade_07/main.py
@@ -0,0 +1,101 @@
+"""
+# TODO - Atividade: Crie um programa que faça as seguintes operações:
+- Cadastre novo nome na lista
+- Liste todos os nomes na lista
+- Pesquise por um nome na lista
+- Altere um nome na lista
+- Exclua um nome na lista
+- Sair do programa
+# NOTE - a lista deve começar vazia. Exemplo: lista= []
+"""
+nomes = []
+while True:
+    # Menu 
+    print(f"{'-'*20} MENU {"-"*20}")
+    print("1 - Cadastre novo nome")
+    print("2 - Liste todos os nomes na lista")
+    print("3 - Pesquise por um nome na lista")
+    print("4 - Altere um nome na lista")
+    print("5 - Exclua um nome na lista")
+    print("6 - Sair do programa")
+    # Informe a opção
+    opcao = input("Informe a operação desejada: ").strip()
+    # lista
+    match opcao:
+        case "1":
+            # Usuário informa nome a ser acrescentado na lista
+            try:
+                novo_nome = input("Informe o novo nome: ").title().strip()
+                nomes.append(novo_nome)
+            except Exception as e:
+                print(f"Não foi possível Infomar o nome. {e}.")
+            finally:
+                for i in range(len(nomes)):
+                    print(f"{i}: {nomes[i]}.")
+                continue
+
+        case "2":
+            # Exibe a lista
+            try:
+                for i in range(len(nomes)):
+                    print(f"{i}: {nomes[i]}.")
+            except Exception as e:
+                print(f"Não foi possível litar a lista. {e}.")
+            finally:
+                for i in range(len(nomes)):
+                    print(f"{i}: {nomes[i]}.")
+                continue
+
+        case "3":
+            try:
+                nome_pesquisada = input("Informe o nome a ser pesquisado: ").title().strip()
+                qtde = nomes.count(nome_pesquisada)
+                print(f"{nome_pesquisada} foi encontrado {qtde} vezes na lista.")
+            except Exception as e:
+                print(f"Não foi possível litar a lista. {e}.")
+            finally:
+                for i in range(len(nomes)):
+                    print(f"{i}: {nomes[i]}.")
+                continue
+
+        case "4":
+            try:
+                # Usuário informa o nome e a posição (índice)
+                novo_nome = input("Informe o nome: ").title().strip()
+                i = int(input("Informe a posição da lista onde deseja inserir: "))
+
+                if i >= 0 and i <= len(nomes):
+                    # Insere novo item em uma posição na lista
+                    nomes.insert(i, novo_nome)
+                    print("Nome inserida com sucesso!")
+                else:
+                    print("Índice inválido.")
+            except Exception as e:
+                print(f"Não foi possível inserir item na lista. {e}.") 
+            finally:
+                # Re-exibe a lista e suas posições
+                for i in range(len(nomes)):
+                    print(f"Índice {i}: {nomes[i]}.")
+
+        case "5":
+            try:
+                i = int(input("Informe a posição do nome na lista: "))
+                if i >= 0 and i < len(nomes):
+                    # Exclui nome da lista
+                    del(nomes[i])
+                    print("Nome excluído com sucesso!")
+                else:
+                    print("Posição inválida.")
+            except Exception as e:
+                print(f"Não foi possível excluir o nome da lista {e}.")
+            finally:
+                for i in range(len(nomes)):
+                    print(f"{i}: {nomes[i]}.")
+                continue
+        case "6":
+            print("Programa encerrado")
+            break
+
+
+
+
