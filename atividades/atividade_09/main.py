ab2d43b97667e81adb86c48dcf8723fbbe61e2e5
gabriel17868 <gabrielmaraschin123@gmail.com>
2025-06-29 13:56:36 -0300

    atividade 09 finalizada

--- atividades/atividade_09/main.py
+++ atividades/atividade_09/main.py
@@ -17,38 +17,53 @@
 - Hora do cadastro
 """
 import os
+import random
 import datetime 
 from datetime import date
+from sys import exception
 
 usuarios = []
 
 while True:
     try:       
         print(f"{'-'*20} MENU {"-"*20}")
-        print("1 - Cadastre um novo usuário.")
-        print("2 - Liste todos os usuários cadastrados")
+        print("1 - Cadastrar novo usuário.")
+        print("2 - Listar todos os usuários cadastrados")
         print("3 - Altere os dados de um usuário")
         print("4 - Sortear um usuário")
-        print("5 - Exclua um usuário")
-        print("6 - Saia do programa")
+        print("5 - Excluir um usuário")
+        print("6 - Sair do programa")
         opcao = input("Informe a opção desejada: ").strip()
         os.system("cls" if os.name =="nt" else "clear")
 
         match opcao:
             case "1":
                 try:
-                    hoje = date.today().strftime("%d/%m/%Y")
+
+                    print("\n--- Cadastro de Novo Usuário ---")
+                    nome = input("Nome completo: ").title().strip()
+                    nascimento = input("Data de nascimento (DD/MM/AAAA): ")
+                    email = input("E-mail: ").strip()
+                    cpf = input("CPF: ").strip()
+                    telefone = input("Telefone: ").strip()
+                    genero = input("Gênero: ").strip()
+                    data = date.today().strftime("%d/%m/%Y")
                     hora = datetime.datetime.now().strftime("%H:%M:%S")
-                    usuario = {}
-                    usuario['nome'] = input("Informe o nome completo: ").strip()
-                    usuario['nascimento'] = input("Informe a data de nascimento: ").strip()
-                    usuario['email'] = input("Informe o E-mail: ").strip()
-                    usuario['CPF'] = input("Informe o CPF: ").strip()
-                    usuario['telefone'] = input("Informe o telefone: ").strip()
-                    usuario['genero'] = input("Informe o Gênero: masculino/feminino ").strip()
-                    usuario['data'] = hoje
-                    usuario['hora'] = hora
+
+                    usuario = {
+                        "nome": nome,
+                        "nascimento": nascimento,
+                        "email": email,
+                        "cpf": cpf,
+                        "telefone": telefone,
+                        "genero": genero,
+                        "data": data,
+                        "hora": hora
+                    }
+
                     usuarios.append(usuario)
+                    print("Usuário cadastrado com sucesso!")
+
                 except Exception as e:
                     print(f"Não foi possível cadastrar usuário. {e}.")
                 finally:
@@ -56,43 +71,82 @@ while True:
 
             case "2":
                 try:
-                    for i in range(len(usuarios)):
-                        print("-"*40)
-                        print(f"Índice {i}: ")
-                        for chave in usuario:
-                            print(f"    {chave.capitalize()}: {usuario.get(chave)}")
+                    if not usuarios:
+                        print("\nNenhum usuário cadastrado.")
+                    else:
+                        print("\n--- Lista de Usuários ---")
+                        for i, usuario in enumerate(usuarios):
+                            print(f"\nÍndice: {i}")
+                            for chave, valor in usuario.items():
+                                print(f"{chave.capitalize().replace('_', ' ')}: {valor}")
                 except Exception as e:
                     print(f"Não foi possível listar os usuários. {e}.")
                 finally:
                     continue
             
-            case "3":
-                try:
-                    sair = "s"
-                    i = int(input("Informe o indice do usuário que deseja alterar: "))
-                    if i >= 0 and i < len(usuarios):
-                        for chave in usuario:
-                            print(f"{chave.capitalize()}: {usuario.get(chave)}")
-                        while sair == "s":
-                            try:
-                                chave = input("Informe a chave que deseja alterar: ").lower().strip()
-                                if chave in usuario:
-                                    usuario[chave] = input(f"Informe o novo valor de {chave}: ").strip()
-                                    print("Chave alterada")
-                                    sair = input("Deseja alterar outra chave: s/n ").lower().strip()
+            case "3":               
+                if not usuarios:
+                    print("\nNenhum usuário cadastrado para alterar.")
+                else:
+                    print("\n--- Lista de Usuários ---")
+                    for i, usuario in enumerate(usuarios):
+                        print(f"{i} - {usuario['nome']}")
+                    try:
+                        indice = int(input("Informe o indice do usuário que deseja alterar: "))
+                        if  0 <= indice < len(usuarios):
+                            continuar = "s"
+                            while continuar == "s":
+                                print("\nCampos disponíveis para alteração:")
+                                for chave in usuarios[indice].keys():
+                                    print(f"{chave.capitalize()}: {usuario.get(chave)}")
+
+                                chave_escolhida = input("Informe a chave que deseja alterar: ").lower().strip()
+                                if chave_escolhida in usuarios[indice]:
+                                    novo_valor = input(f"Informe o novo valor de {chave_escolhida}: ").strip()
+                                    usuarios[indice][chave_escolhida] = novo_valor
+                                    print(f"Campo '{chave_escolhida}' alterado com sucesso!")                                    
                                 else:
-                                    print("Chave inexistente.")
-                                
-                            except Exception as e:
-                                print(f"Não foi possível lista o usuário. {e}.")
-                            finally:
-                                continue
-                    
+                                    print("Campo inválido.")
+                                continuar = input("Deseja alterar outra chave: s/n ").lower().strip()      
+                        else:       
+                            print("Índice inválido.")    
+                    except Exception as e:
+                        print(f"Não foi possível listar os usuários. {e}.")
+                    finally:
+                        continue
+            
+            case "4":
+                try:
+                    if not usuarios:
+                        print("\nNenhum usuário cadastrado para sortear.")
+                    else:
+                        i = random.randint(0, len(usuarios)-1)
+                        print(f"Usuário sorteado: {i} - {usuarios[i]['nome']}")
                 except Exception as e:
-                    print(f"Não foi possível listar os usuários. {e}.")
+                    print(f"Não foi possível sortear os usuários. {e}.")
                 finally:
                     continue
                 
+            case "5":
+                try:
+                    if not usuarios:
+                        print("\nNenhum usuário cadastrado para alterar.")
+                    else:
+                        print("\n--- Lista de Usuários ---")
+                        for i, usuario in enumerate(usuarios):
+                            print(f"{i} - {usuario['nome']}")
+                        indice = int(input("Informe o indice do usuário que deseja excluir: "))
+                        if  0 <= indice < len(usuarios):
+                            nome_excluido = usuarios[indice]['nome']
+                            del(usuarios[indice])
+                            print(f"Usuário {nome_excluido} excluído com sucesso!")
+                        else:
+                            print("Índice inválido.")
+                except Exception as e:
+                    print(f"Não foi possível excluir o usuário. {e}.")
+                finally:
+                    continue
+
             case "6":
                 print("Programa encerrado")
                 break
