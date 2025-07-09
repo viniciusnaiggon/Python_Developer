5f4a8fd90a8ef94b41a5b67b28e8d383b9f37f42
gabriel17868 <gabrielmaraschin123@gmail.com>
2025-07-09 13:48:50 -0300

    Atividade 10 completo

--- atividades/atividade_10/main.py
+++ atividades/atividade_10/main.py
@@ -22,12 +22,14 @@ import json
 
 usuarios = []
 arquivo = ""
+diretorio = ""
+
 while True:
     try:
-        
+        os.system("cls" if os.name == "nt" else "clear")
         # Menu 
         if not arquivo:
-            print("Nenhum aquivo aberto")
+            print("Nenhum arquivo aberto")
         else:
             print(f"O arquivo {arquivo} está aberto")
             
@@ -41,7 +43,6 @@ while True:
         print("7 - Deletar usuário de um arquivo JSON.")
         print("8 - Sair do programa.")
         opcao = input("Informe a opção desejada: ").strip()
-        os.system("cls" if os.name =="nt" else "clear")
 
         match opcao:
             case "1":
@@ -52,6 +53,7 @@ while True:
                     json.dump(usuarios, f, ensure_ascii=False, indent=4)
                 os.system("cls" if os.name == "nt" else "clear")
                 print("Arquivo criado com sucesso.")
+                input("\nPressione Enter para continuar...")
             
             case "2":
                 arquivo = input("Informe o nome do arquivo: ").strip().lower()
@@ -61,6 +63,7 @@ while True:
                     usuarios = dados
                 os.system("cls" if os.name == "nt" else "clear")
                 print("Arquivo aberto com sucesso")     
+                input("\nPressione Enter para continuar...")
 
             case "3":
                 try:
@@ -81,21 +84,25 @@ while True:
                     print("Usuário cadastrado com sucesso.")            
                 except Exception as e:
                     print(f"Não foi possível cadastrar usuário. {e}.")
+                input("\nPressione Enter para continuar...")
             
             case "4":
                 if not usuarios:
                     print("Nenhum usuário cadastrado.")
                 else:
-                    with open(f"{diretorio}/{arquivo}.json", "r", encoding="utf-8") as f:
-                        dados = json.load(f)
                     print(f"{'-'*20} USUÁRIOS {'-'*20}")
-                    for i, usuario in enumerate(dados):
-                        print(f"\nÍndice: {i}")
-                        for chave in usuario:
-                            print(f"    {chave.capitalize()}: {usuario.get(chave)}")
-                        print(f"{'-'*50}")
+                    for i, usuario in enumerate(usuarios):
+                        try:
+                            print(f"\nÍndice: {i}")
+                            for chave, valor in usuario.items():
+                                print(f"    {chave.capitalize()}: {valor}")
+                            print(f"{'-'*50}")
+                        except Exception as e:
+                            print(f"Erro ao exibir usuário no índice {i}: {e}")
+                input("\nPressione Enter para continuar...")
             
             case "5":   
+                os.system("cls" if os.name =="nt" else "clear")
                 chave = input("Informe a chave para buscar (ex: nome, cpf, email): ").lower()
                 valor = input("Informe o valor para buscar: ")
 
@@ -112,11 +119,12 @@ while True:
                             print(f"{chave.capitalize()}: {valor}")
                 else:
                     print("Nenhum usuário encontrado com esse valor.")
-
+                input("\nPressione Enter para continuar...")
 
             case "6":
                 if not usuarios:
                     print("\nNenhum usuário cadastrado para alterar.")
+                    input("\nPressione Enter para continuar...")
                     continue
                 try:
                     indice = int(input("Informe o índice do usuário a ser alterado: "))
@@ -124,35 +132,46 @@ while True:
                         usuario_alterado = usuarios[indice]
                         print("Usuário encontrado. Deixe em branco para manter o valor atual.")
                         usuario_alterado["nome"] = input(f"Nome ({usuario_alterado['nome']}): ") or usuario_alterado["nome"]
-                        usuario_alterado["data_nascimento"] = input(f"Data de nascimento ({usuario_alterado['nascimento']}): ") or usuario_alterado["nascimento"]
+                        usuario_alterado["data_nascimento"] = input(f"Data de nascimento ({usuario_alterado['data_nascimento']}): ") or usuario_alterado["data_nascimento"]
+                        usuario_alterado["CPF"] = input(f"CPF ({usuario_alterado['CPF']}): ") or usuario_alterado["CPF"]
                         usuario_alterado["email"] = input(f"E-mail ({usuario_alterado['email']}): ") or usuario_alterado["email"]
                         usuario_alterado["telefone"] = input(f"Telefone ({usuario_alterado['telefone']}): ") or usuario_alterado["telefone"]
                         usuario_alterado["filme"] = input(f"Filme favorito ({usuario_alterado['filme']}): ") or usuario_alterado["filme"]
+                        with open(f"{diretorio}/{arquivo}.json", "w", encoding="utf-8") as f:
+                            json.dump(usuarios, f, ensure_ascii=False, indent=4)
+                        print("Usuário alterado com sucesso.")
                 except Exception as e:
-                    print(f"Não foi possível cadastrar usuário. {e}.")
+                    print(f"Não foi possível alterar os dados do usuário. {e}.")
+                input("\nPressione Enter para continuar...")
 
             case "7":
-                if not arquivo:
-                    print("Nenhum arquivo aberto")
                 if not usuarios:
-                    print("\nNenhum usuário cadastrado para alterar.")
-                else:
-                    print("\n--- Lista de Usuários ---")
-                    for i, usuario in enumerate(usuarios):
-                        print(f"{i} - {usuario['nome']}")
-
+                    print("\nNenhum usuário cadastrado para excluir.")
+                    continue
+                try:
+                    indice = int(input("Informe o índice do usuário a ser excluido: "))
+                    if 0 <= indice < len(usuarios):
+                        nome_excluido = usuarios[indice]["nome"]
+                        del(usuarios[indice])
+                        print(f"Usuário {nome_excluido} excluído com sucesso!")
+                        with open(f"{diretorio}/{arquivo}.json", "w", encoding="utf-8") as f:
+                            json.dump(usuarios, f, ensure_ascii=False, indent=4)
+                        
+                except Exception as e:
+                    print(f"Não foi possível excluir o usuário. {e}.")
+                input("\nPressione Enter para continuar...")
             
             case "8":
                 print("Programa encerrado")
                 break
             
             case _:
-                os.system("cls" if os.name =="nt" else "clear")
                 print("Opção Inválida")
+                input("\nPressione Enter para continuar...")
                 continue  
-
 
     except Exception as e:
         print(f"Não foi possível realizar requisição. {e}.")
+        input("\nPressione Enter para continuar...")
 
 
