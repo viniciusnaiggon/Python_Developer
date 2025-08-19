875da229547a9f8fd5dece19744709b57fbb397d
viniciusnaiggon <pedro.s.aguiar@aluno.senai.br>
2025-08-18 21:34:00 -0300

    aula dia 19/08

---
+++ orientacao_a_objetos/13_panda/main.py
@@ -0,0 +1,13 @@
+# Tupla, diferença da lista é que não pode modificar e excluir. é imutável
+dias_da_semana = (
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feita",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sábado"  
+)
+
+for dia in dias_da_semana:
+    print(dia)
