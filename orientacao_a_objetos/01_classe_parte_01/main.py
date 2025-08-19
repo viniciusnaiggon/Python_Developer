9365b850a0d3942fb0bf21dd5164f2b48473718b
viniciusnigga <vinicius.gmachado@sempreceub.com>
2025-08-18 21:40:26 -0300

    aula dia 19/08

--- orientacao_a_objetos/01_classe_parte_01/main.py
+++ orientacao_a_objetos/01_classe_parte_01/main.py
@@ -1,21 +1,41 @@
-# O py tá on
-# Classe
+# classe
 class Pessoa:
-    # Atributos
-    nome = "Gabriel Maraschin"
-    idade = 27
-    telefone = "(61) 99999-9999"
-    cpf = "123.456.789-12"
-    email = "gabriel@gmail.com"
+    # construtor
+    def __init__(self, nome, idade, telefone, cpf, email):
+        # atributos
+        self.nome = nome
+        self.idade = idade
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
 
-    # Métodos
+    # Método de ação
     def apresentar(self):
-        print(f"Olá, o meu nome é {self.nome}, tenho {self.idade} anos, meu telefone é {self.telefone}, meu CPF é {self.cpf} e meu e-mail é {self.email}.")
-
-# Programa principal
+        return f"Olá, o meu nome é {self.nome}, tenho {self.idade} anos, meu telefone é {self.telefone}, meu CPF é {self.cpf} e meu e-mail é {self.email}."
+    
+# algoritmo principal
 if __name__ == "__main__":
-    # Instanciar classe
-    usuario = Pessoa()
+    # instancia a classe
+    usuario = Pessoa(
+        nome="",
+        idade=0,
+        telefone="",
+        cpf="",
+        email=""
+    )
 
-    # Objeto se apresenta
+    try: 
+        # input
+        usuario.nome = input("Informe seu nome: ").strip().title()
+        usuario.idade = int(input("Informe sua idade: "))
+        usuario.telefone = input("Informe seu telefone: ").strip()
+        usuario.cpf = input("Informe seu cpf: ").strip()
+        usuario.email = input("Informe seu email: ").strip().lower()    
+
+        # chama o método
+        print(usuario.apresentar())
+        
+    except Exception as e:
+        print(f"Erro. {e}.")
+
     usuario.apresentar()
